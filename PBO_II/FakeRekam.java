20d1a9a14cb11009b5149988d58edee906372b54
Aldy Satria Gumilar 056 <pilusnarziz3@gmail.com>
2015-04-25 10:16:26 +0700

    ufvctxh vbn

--- PBO_II/FakeRekam.java
+++ PBO_II/FakeRekam.java
@@ -2,6 +2,7 @@ package PBO_II;
 
 public class FakeRekam {
 	public static void main (String [] args){
-		System.out.println("Hallo Gopek");	
+		System.out.println("Hallo Gopek");
+		System.out.println("-_-");
 	}
 }
