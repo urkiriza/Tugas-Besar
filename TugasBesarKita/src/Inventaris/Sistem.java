9688c3645cd29c9718d7111663fc4c7d9009ead5
Aldy Satria Gumilar 056 <pilusnarziz3@gmail.com>
2015-05-30 07:12:24 +0700

    dulala
    Signed-off-by: Aldy Satria Gumilar 056 <pilusnarziz3@gmail.com>

--- TugasBesarKita/src/Inventaris/Sistem.java
+++ TugasBesarKita/src/Inventaris/Sistem.java
@@ -1,5 +1,18 @@
 package Inventaris;
 
 public abstract class Sistem {
+	public abstract int HitungLuasRuang();
+	public abstract int bentukRuang();
+	public abstract Double HitungRasioRuangan();
+	public abstract String AnalisisPdanJ();
+	public abstract String AnalisisListrik();
+	public abstract String AnalisisProyektor();
+	public abstract String AnalisisLampu();
+	public abstract String AnalisisKipasAngin();
+	public abstract String AnalisisAC();
+	public abstract String AnalisisInternet();
+	public abstract String AnalisisCCTV();
+	public abstract String AnalisisKebersihan();
+	public abstract String AnalisisSirUdara();
 	
 }
