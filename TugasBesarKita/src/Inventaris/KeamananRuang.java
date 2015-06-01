8633027091a8f110f506c9bd5109f2755ada80bc
rzqk <rizqikar11@gmail.com>
2015-06-02 00:08:01 +0700

    Update KeamananRuang.java

--- TugasBesarKita/src/Inventaris/KeamananRuang.java
+++ TugasBesarKita/src/Inventaris/KeamananRuang.java
@@ -8,19 +8,53 @@ abstract public class KeamananRuang extends Sistem{
 	
 	@Override
 	String AnalisisKekokohan() {
-		// TODO Auto-generated method stub
+		System.out.println("Kekokohan = " + AnalisisKekokohan());
+		String KK = "Kokoh";
+		if ("Kokoh".equals(KK))
+			System.out.println("Kondisi Sesuai");
+		else if("ada".equals(KK))
+			System.out.println("Kondisi Tidak sesuai");
+		else
+			System.out.println("Input Salah");
 		return null;
 	}
 
 	@Override
-	String AnalisisKunci() {
-		// TODO Auto-generated method stub
+	String AnalisisKunciPintu() {
+		System.out.println("Analisis Kunci = " + AnalisisKunciPintu());
+		String KP = "Ada";
+		if("Ada".equals(KP))
+			System.out.println("Kondisi Sesuai");
+		else if("Tidak Ada".equals(KP))
+			System.out.println("Kondisi Tidak sesuai");
+		else
+			System.out.println("Input Salah");
+		return null;
+	}
+
+	@Override
+	String AnalisisKunciJendela() {
+		System.out.println("Analisis Jendela = " + AnalisisKunciJendela());
+		String KJ = "Ada";
+		if ("Ada".equals(KJ))
+			System.out.println("Kondisi Sesuai");
+		else if("Tidak Ada".equals(KJ))
+			System.out.println("Kondisi Tidak sesuai");
+		else
+			System.out.println("Input Salah");
 		return null;
 	}
 
 	@Override
 	String AnalisisKeamananRuang() {
-		// TODO Auto-generated method stub
+		System.out.println("Analisis Keamanan Ruangan = " + AnalisisKeamananRuang());
+		String KR = "Aman";
+		if ("Aman".equals(KR))
+			System.out.println("Kondisi Sesuai");
+		else if("Tidak Aman".equals(KR))
+			System.out.println("Kondisi Tidak sesuai");
+		else
+			System.out.println("Input Salah");
 		return null;
 	}
 }
