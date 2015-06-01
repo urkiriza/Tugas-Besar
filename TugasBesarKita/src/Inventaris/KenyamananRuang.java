49dea1089d66d2cde695ad55d071c7276f2972d4
Bima Sakhi Fadilat <bfadilat@gmail.com>
2015-06-02 01:01:02 +0700

    Update KenyamananRuang.java

--- TugasBesarKita/src/Inventaris/KenyamananRuang.java
+++ TugasBesarKita/src/Inventaris/KenyamananRuang.java
@@ -8,31 +8,66 @@ abstract public class KenyamananRuang extends Sistem{
 	
 	@Override
 	String AnalisisKebisingan() {
-		// TODO Auto-generated method stub
+		System.out.println("Analisis Kebisingan = " + AnalisisKebisingan());
+		String K = "Tidak Bising";
+		if ("Tidak Bising".equals(K))
+			System.out.println("Kondisi Sesuai");
+		else if ("Bising".equals(K))
+			System.out.println("Kondisi Tidak Sesuai");
+		else
+			System.out.println("Input Salah");
 		return null;
 	}
 
 	@Override
 	String AnalisisBau() {
-		// TODO Auto-generated method stub
+		System.out.println("Analisis Bau = " + AnalisisBau());
+		String B = "Tidak Bau";
+		if ("Tidak Bau".equals(B))
+			System.out.println("Kondisi Sesuai");
+		else if ("Bau".equals(B))
+			System.out.println("Kondisi Tidak Sesuai");
+		else
+			System.out.println("Input Salah");
 		return null;
 	}
 
 	@Override
 	String AnalisisKebocoran() {
-		// TODO Auto-generated method stub
+		System.out.println("Analisis Kebocoran = " + AnalisisKebocoran());
+		String C = "Tidak Ada";
+		if ("Tidak Ada".equals(C))
+			System.out.println("Kondisi Sesuai");
+		else if ("Ada".equals(C))
+			System.out.println("Kondisi Tidak Sesuai");
+		else
+			System.out.println("Input Salah");
 		return null;
 	}
 
 	@Override
 	String AnalisisKerusakan() {
-		// TODO Auto-generated method stub
+		System.out.println("Analisis Kerusakan = " + AnalisisKerusakan());
+		String R = "TidaK Ada";
+		if ("Tidak Ada".equals(R))
+			System.out.println("Kondisi Sesuai");
+		else if ("Ada".equals(R))
+			System.out.println("Kondisi Tidak Sesuai");
+		else 
+			System.out.println("Input Salah");
 		return null;
 	}
 
 	@Override
 	String AnalisisKeausan() {
-		// TODO Auto-generated method stub
+	System.out.println("Analisis Keausan =" + AnalisisKeausan());
+	String A = "Tidak Ada";
+	if ("Tidak Ada".equals(A))
+		System.out.println("Kondisi Sesuai");
+	else if("ada".equals(A))
+		System.out.println("Kondisi Tidak sesuai");
+	else
+		System.out.println("Input Salah");
 		return null;
 	}
 }
