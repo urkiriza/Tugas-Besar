327f23bc46e06f0655c0ce221d591fefaac937c6
Bima Sakhi Fadilat <bfadilat@gmail.com>
2015-06-02 01:01:52 +0700

    Update LingkunganRuang.java

--- TugasBesarKita/src/Inventaris/LingkunganRuang.java
+++ TugasBesarKita/src/Inventaris/LingkunganRuang.java
@@ -7,8 +7,67 @@ abstract public class LingkunganRuang extends Sistem{
 	}
 	
 	@Override
-	String AnalisisKebersihan() {
-		// TODO Auto-generated method stub
+	String AnalisisKondisiLantai() {
+		System.out.println("Analisis Kondisi Lantai = " + AnalisisKondisiLantai());
+		String KL = "Bersih";
+		if("Bersih".equals(KL))
+			System.out.println("Sesuai");
+		else if("Kotor".equals(KL))
+			System.out.println("Tidak Sesuai");
+		else 
+			System.out.println("Input Salah");
+		return null;
+	}
+
+	@Override
+	String AnalisisKondisiDinding() {
+	System.out.println("Analisis Kondisi DInding = " + AnalisisKondisiDinding());
+	String KD = "Bersih";
+	if("Bersih".equals(KD))
+		System.out.println("Sesuai");
+	else if("Kotor".equals(KD))
+		System.out.println("Tidak Sesuai");
+	else 
+		System.out.println("Input Salah");
+		return null;
+	}
+
+	@Override
+	String AnalisisKondisiAtap() {
+		System.out.println("Analisis Kondisi Atap = " + AnalisisKondisiAtap());
+		String KA = "Bersih";
+		if("Bersih".equals(KA))
+			System.out.println("Sesuai");
+		else if("Kotor".equals(KA))
+			System.out.println("Tidak Sesuai");
+		else 
+			System.out.println("Input Salah");
+		return null;
+	}
+
+	@Override
+	String AnalisisKondisiPintu() {
+	System.out.println("Analisis Kondisi Pintu = " + AnalisisKondisiPintu());
+	String KP = "Bersih";
+	if("Bersih".equals(KP))
+		System.out.println("Sesuai");
+	else if("Kotor".equals(KP))
+		System.out.println("Tidak Sesuai");
+	else 
+		System.out.println("Input Salah");
+		return null;
+	}
+
+	@Override
+	String AnalisisKondisiJendela() {
+		System.out.println("Analisis Kondisi Jendela = " + AnalisisKondisiJendela());
+		String KJ = "Bersih";
+		if("Bersih".equals(KJ))
+			System.out.println("Sesuai");
+		else if("Kotor".equals(KJ))
+			System.out.println("Tidak Sesuai");
+		else 
+			System.out.println("Input Salah");
 		return null;
 	}	
 }
