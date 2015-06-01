a0396a9c85f3567009ddd5cb760947f01a35ccf9
rzqk <rizqikar11@gmail.com>
2015-06-02 00:05:48 +0700

    Update KondisiRuang.java

--- TugasBesarKita/src/Inventaris/KondisiRuang.java
+++ TugasBesarKita/src/Inventaris/KondisiRuang.java
@@ -5,27 +5,53 @@ abstract public class KondisiRuang extends Sistem{
 	public KondisiRuang(){
 		super();
 	}
+	
 	@Override
-	int HitungLuasRuang() {
-		// TODO Auto-generated method stub
+	double HitungLuasRuang(int HitungLuasRuang) {
+		System.out.println("Luas Ruangan = " + HitungLuasRuang());
+		HitungLuasRuang = getPanjangRuang() * getLebarRuang();
 		return 0;
 	}
 
 	@Override
-	int bentukRuang() {
-		// TODO Auto-generated method stub
+	double BentukRuang() {
+		System.out.println("Bentuk Ruangan = " + BentukRuang());
+		String PJ = "Persegi Panjang";
+		if ((getPanjangRuang()) != (HitungLuasRuang())){
+			System.out.println(PJ);}
+		else if((getPanjangRuang())==(HitungLuasRuang())){
+			System.out.println("Tidak persegi panjang");
+			System.out.println("Tidak Sesuai");
+			}
+		else {
+			System.out.println("Bentuk Ruangan Absurd");
+			}
+		
 		return 0;
 	}
 
 	@Override
-	Double HitungRasioRuangan() {
-		// TODO Auto-generated method stub
-		return null;
+	double HitungRasioRuangan(double HitungRasioLuas) {
+		System.out.println("Rasio Luas Ruangan = " + HitungRasioRuangan());
+		HitungRasioLuas = HitungLuasRuang() / getJumlahKursi();
+		if (HitungRasioLuas >= 0.5){
+			System.out.println("Sesuai");
+		}
+		else if(HitungRasioLuas <= 0.5){
+			System.out.println("Input Salah");
+		}
+		return 0;
 	}
 
 	@Override
 	String AnalisisPdanJ() {
-		// TODO Auto-generated method stub
+		System.out.println("Analisis Pintu dan Jendela = " + AnalisisPdanJ());
+		if ((getJumlahPintu() >= 2) && ((getJumlahJendela() >= 1))){
+			System.out.println("Sesuai");
+		}
+		else if ((getJumlahPintu() <= 1) && ((getJumlahJendela() <= 1))){
+			System.out.println("Tidak Sesuai");
+		}
 		return null;
 	}	
 }
