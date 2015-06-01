a753ac97488bbad35ce706be70ea9a04941bcd4e
rzqk <rizqikar11@gmail.com>
2015-06-02 00:08:38 +0700

    Update KebersihanRuang.java

--- TugasBesarKita/src/Inventaris/KebersihanRuang.java
+++ TugasBesarKita/src/Inventaris/KebersihanRuang.java
@@ -8,25 +8,53 @@ abstract public class KebersihanRuang extends Sistem{
 	
 	@Override
 	String AnalisisSirUdara() {
-		// TODO Auto-generated method stub
+		System.out.println("Analisis Sirkulasi Udara = " + AnalisisSirUdara());
+		String SU = "Terbuka";
+		if ("Terbuka".equals(SU))
+			System.out.println("Sesuai");
+		else if ("Tertutup".equals(SU))
+			System.out.println("Tidak Sesuai");
+		else
+			System.out.println("Input Salah");
 		return null;
 	}
 
 	@Override
 	String AnalisisPencahayaan() {
-		// TODO Auto-generated method stub
+		System.out.println("Analisis Pencahayaan = " + AnalisisPencahayaan());
+		int NP = 250;
+		if ((NP >= 250) && (NP<=350))
+			System.out.println("Cahaya Sesuai");
+		else if ((NP >= 1) && (NP <= 249))
+			System.out.println("Cahaya Tidak Sesuai");
+		else
+			System.out.println("Input Salah");
 		return null;
 	}
 
 	@Override
 	String AnalisisKelmabapan() {
-		// TODO Auto-generated method stub
+		System.out.println("Analisis Kelembapan = " + AnalisisKelmabapan());
+		int K = 70;
+		if ((K >= 70) && (K <= 80))
+			System.out.println("Lembab Sesuai");
+		else if ((K >= 1) && (K <= 69))
+			System.out.println("Lembab Tidak Sesuai");
+		else
+			System.out.println("Input Salah");
 		return null;
 	}
 
 	@Override
 	String AnalisisSuhu() {
-		// TODO Auto-generated method stub
+		System.out.println("Analisis Suhu = " + AnalisisSuhu());
+		int S = 25;
+		if ((S >= 25) && (S <= 	35))
+			System.out.println("Suhu Sesuai");
+		else if ((S >= 1) && (S <= 24))
+			System.out.println("Suhu Tidak Sesuai");
+		else
+			System.out.println("Input Salah");
 		return null;
 	}
 }
