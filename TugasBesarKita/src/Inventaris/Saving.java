adc489265d91e715dfa2ec7dd9e38a147a26a5dd
Aldy Satria Gumilar 056 <pilusnarziz3@gmail.com>
2015-06-06 08:20:32 +0700

    Signed-off-by: Aldy Satria Gumilar 056 <pilusnarziz3@gmail.com>

--- TugasBesarKita/src/Inventaris/Saving.java
+++ TugasBesarKita/src/Inventaris/Saving.java
@@ -1,7 +1,9 @@
 package Inventaris;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class Saving  {
@@ -23,5 +25,28 @@ public class Saving  {
 	      }
 
 		return save();
+	}
+	public Object Read(){
+		RekapRuang RR= new RekapRuang();
+	      try
+	      {
+	         FileInputStream fileIn = new FileInputStream("/tmp/employee.ser");
+	         ObjectInputStream in = new ObjectInputStream(fileIn);
+	         RR = (RekapRuang) in.readObject();
+	         in.close();
+	         fileIn.close();
+	      }catch(IOException i)
+	      {
+	         i.printStackTrace();
+	         return i;
+	      }catch(ClassNotFoundException c)
+	      {
+	         System.out.println("RekapRuang class not found");
+	         c.printStackTrace();
+	         return c;
+	      }
+	      RR.Ruangan();
+
+	      return Read();
 	}
 }
